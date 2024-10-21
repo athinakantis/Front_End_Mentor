832370b1b626309bc5c057c7be80b5866b16733c
Athina Kantis <[athina.kantis@gmail.com]>
2024-10-21 19:05:42 +0300

    add video preview functionality

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 const preview = document.querySelector('#preview')
+const videoPreview = document.querySelector('#videoPreview')
 const challenges = [
-    'assets/previews/desktopView_Athina_Kantis.png'
+    'assets/previews/desktopView_Athina_Kantis.png',
+    'assets/previews/Launch_Countdown_Preview.mp4'
 ]
 
 
-const centerX = 556;
-const centerY = 664;
-
-
-function previewChallenge(challenge, e) {
-    e.preventDefault()
-    const clientRect = e.target.getBoundingClientRect()
+function previewChallenge(challenge, event) {
+    event.preventDefault()
+    const clientRect = event.target.getBoundingClientRect()
     const clientX = clientRect.left;
     const clientY = clientRect.top;
 
     preview.src = challenges[challenge];
     preview.style.opacity = 1;
-    preview.style.transform = `translate(${e.clientX - clientX}px, ${e.clientY - clientY}px)`
+    preview.style.transform = `translate(${event.clientX - clientX}px, ${event.clientY - clientY}px)`
 }
 
+function previewVideo(video) {
+    videoPreview.src = video
+    videoPreview.style.opacity = 1;
+}
+
+function animateVideo(event) {
+    event.preventDefault()
+    const clientRect = event.target.getBoundingClientRect()
+    const clientX = clientRect.left;
+    const clientY = clientRect.top;
+    videoPreview.style.transform = `translate(${event.clientX - clientX}px, ${event.clientY - clientY}px)`;
+}
 
 const bentoGridBtn = document.querySelector('#bentoGrid')
 bentoGridBtn.addEventListener('mousemove', (e) => previewChallenge(0, e))
 bentoGridBtn.addEventListener('mouseout', () => {
     preview.style.opacity = 0;
+})
+
+const launchCountdown = document.querySelector('#launchCountdown');
+launchCountdown.addEventListener('mouseenter', () => previewVideo('assets/previews/Launch_Countdown_Preview.mp4'))
+launchCountdown.addEventListener('mousemove', (e) => animateVideo(e));
+launchCountdown.addEventListener('mouseout', () => {
+    videoPreview.style.opacity = 0
 })
