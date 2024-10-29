ddd56b65e54a174fda72f45c7d1b9425678d190a
Athina Kantis <[athina.kantis@gmail.com]>
2024-10-29 13:23:55 +0200

    add select plan styling

--- multiStepForm/src/Pages/SelectPlan.jsx
+++ multiStepForm/src/Pages/SelectPlan.jsx
@@ -14,29 +14,29 @@ function SelectPlan(props) {
 
             <div className="plans">
 
-            <div className="planCard" onClick={() => setGamePlan('Arcade')}>
+            <div className={`planCard ${gamePlan === 'Arcade' ? 'active' : 'inactive'}`} onClick={() => setGamePlan('Arcade')}>
                 <img src="../assets/images/icon-arcade.svg" alt="Image of arcade video game controller" />
 
                 <div className="description">
                 <p className="title">Arcade</p>
                 <p className="pricing">{!props.currentPlan ? '$9/mo' : '$90/yr'}</p>
-                {props.currentPlan && <p>2 months free</p>}
+                {props.currentPlan && <p className='free'>2 months free</p>}
                 </div>
             </div>
-            <div className="planCard" onClick={() => setGamePlan('Advanced')}>
+            <div className={`planCard ${gamePlan === 'Advanced' ? 'active' : 'inactive'}`} onClick={() => setGamePlan('Advanced')}>
                 <img src="../assets/images/icon-advanced.svg" alt="Image of handheld video game controller" />
                 <div className="description">
                 <p className="title">Advanced</p>
                 <p className="pricing">{!props.currentPlan ? '$12/mo' : '$120/yr'}</p>
-                {props.currentPlan && <p>2 months free</p>}
+                {props.currentPlan && <p className="free">2 months free</p>}
                 </div>
             </div>
-            <div className="planCard" onClick={() => setGamePlan('Pro')}>
+            <div className={`planCard ${gamePlan === 'Pro' ? 'active' : 'inactive'}`} onClick={() => setGamePlan('Pro')}>
                 <img src="../assets/images/icon-pro.svg" alt="Image of a third controller" />
                 <div className="description">
                 <p className="title">Pro</p>
                 <p className="pricing">{!props.currentPlan ? '$15/mo' : '$150/yr'}</p>
-                {props.currentPlan && <p>2 months free</p>}
+                {props.currentPlan && <p className="free">2 months free</p>}
                 </div>
             </div>
             </div>
